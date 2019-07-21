6bbc9321e218edb6bd48b12c38f48cfaa9fb0ae1
Soukarya Ghosh <sg4fz@virginia.edu>
2019-07-21 13:58:58 -0400

    sherlocked

---
+++ sherlock_and_divisors.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+
+#
+# Complete the divisors function below.
+#
+def divisors(n):
+    count = 0
+    for x in range(1, int(n**0.5)+1):
+        hardDiv = n//x
+        if n % x == 0 and x != hardDiv and x % 2 == 0:
+            count += 1
+        if n % (hardDiv) == 0 and (hardDiv) % 2 == 0:
+            count += 1
+
+    return count
+    
+
+if __name__ == '__main__':
+    results = []
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = divisors(n)
+        results.append(result)
+
+    print(results)
